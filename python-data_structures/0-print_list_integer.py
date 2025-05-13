1a55c6ca9c1cb28e2d019146dea407158eaad212
ItsZmainDev <zmainpro@gmail.com>
2025-05-14 00:23:29 +0200

    Task 0 - Correction

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    index = 0
-    while index < len(my_list):
-        print("{}".format(my_list[index]))
-        index = index + 1
+    for number in my_list:
+        print("{:d}".format(number))
