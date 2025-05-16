c3c4a32027ce5c32bf0cfb82031df6020845df9d
ItsZmainDev <zmainpro@gmail.com>
2025-05-16 22:54:26 +0200

    Fix code style

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-Module function to add two intergers.
+Module function to add two integers.
 """
+
 
 def add_integer(a, b=98):
     """
     Adds two integers or floats (floats are cast to integers).
-    
+
     Args:
         a: First number (int or float)
         b: Second number (int or float, default = 98)
-    
+
     Returns:
         int: The sum of a and b after casting to integers
 
@@ -21,5 +22,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
