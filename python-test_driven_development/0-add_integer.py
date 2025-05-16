e41da8eeb045e10b3e84098ef1945e2ef159f1c5
ItsZmainDev <zmainpro@gmail.com>
2025-05-16 23:02:06 +0200

    Fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,18 +17,9 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: If a or b is not an int or float
-        ValueError: If a or b is NaN or infinity
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float) and a != a:
-        raise ValueError("a must be a finite number")
-    if isinstance(b, float) and b != b:
-        raise ValueError("b must be a finite number")
-    if isinstance(a, float) and (a == float('inf') or a == -float('inf')):
-        raise ValueError("a must be a finite number")
-    if isinstance(b, float) and (b == float('inf') or b == -float('inf')):
-        raise ValueError("b must be a finite number")
     return int(a) + int(b)
