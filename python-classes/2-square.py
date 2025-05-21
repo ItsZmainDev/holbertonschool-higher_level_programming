e08b237bf63309bc99160eb093244df1065594bc
ItsZmainDev <zmainpro@gmail.com>
2025-05-21 22:01:47 +0200

    Task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a Square class with size"""
+
+
+class Square:
+    """Square class with size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("Size if not an integer")
+        elif size < 0:
+            raise ValueError("size is not >= 0")
+        self.__size = size
