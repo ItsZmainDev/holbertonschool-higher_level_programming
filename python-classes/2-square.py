545529b38c0ced5902a717978f8cfb0826caa979
ItsZmainDev <zmainpro@gmail.com>
2025-05-21 22:03:49 +0200

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,10 +3,10 @@
 
 
 class Square:
-    """Square class with size"""
+    """Square class with private size attribute"""
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError("Size if not an integer")
+            raise TypeError("size is not an integer")
         elif size < 0:
             raise ValueError("size is not >= 0")
         self.__size = size
