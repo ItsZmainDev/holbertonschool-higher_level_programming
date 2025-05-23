95dd52db681faef45a963242c9498500384411e0
ItsZmainDev <zmainpro@gmail.com>
2025-05-23 11:20:42 +0200

    Task 1 - Correction

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,20 +3,34 @@
 
 
 class Rectangle:
+    """Rectangle class with properties and methods"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-    def width(self, value):
-        if isinstance(value, (int)):
-            raise TypeError("width must be an integer")
-        if value >= 0:
-            raise ValueError("width must be >= 0")
-        self.width = value
+    @property
+    def width(self):
+        return self.__width
 
+    @width.setter
+    def width(self, value):
+        """Function to set the new width value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
     def height(self, value):
-        if isinstance(value, (int)):
+        """Function to set the new height value"""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value >= 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.width = value
+        self.__height = value
