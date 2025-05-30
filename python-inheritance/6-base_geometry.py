19a0ca02ad015135c1d9838b51fbf83c15093c07
ItsZmainDev <zmainpro@gmail.com>
2025-05-30 14:26:28 +0200

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Base Geometry class module"""
+
+
+class BaseGeometry:
+    """Base Geometry class with area method"""
+    def area(self):
+        raise Exception("area() is not implemented")
