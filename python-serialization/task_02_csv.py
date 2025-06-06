e63e02a82c7eb8644e726ad2713e9276acfc59be
ItsZmainDev <zmainpro@gmail.com>
2025-06-06 23:18:43 +0200

    Fix a json data

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -9,11 +9,11 @@ def convert_csv_to_json(csv_filename):
     """Serialize CSV to JSON"""
     try:
         with open(csv_filename, "r") as csv_f:
-            reader = csv.DictReader(csv_file)
+            reader = csv.DictReader(csv_f)
             data = [row for row in reader]
 
         with open("data.json", "w") as json_f:
-            json.dump(csv_dict, json_f)
+            json.dump(data, json_f)
 
         return True
 
