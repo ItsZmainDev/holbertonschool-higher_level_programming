8366adc58997c98326d045faf1f405f2a2877639
root <zmainpro@gmail.com>
2025-06-15 23:40:01 +0200

    Fix not found return message

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -36,7 +36,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 def run(server_class=http.server.HTTPServer,
