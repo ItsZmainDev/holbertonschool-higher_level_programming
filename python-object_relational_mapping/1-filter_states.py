ac65263534651c228946a9f79f5ba3856b04bbcd
root <zmainpro@gmail.com>
2025-06-27 16:21:18 +0200

    Fix pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa"""
+"""
+Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
@@ -17,7 +19,9 @@ if __name__ == "__main__":
         db=db_name
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
