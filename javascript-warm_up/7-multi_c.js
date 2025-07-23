d9f3fce3156e0a56c02ae195944aac955d050391
root <zmainpro@gmail.com>
2025-07-23 15:40:26 +0200

    Task 7 - Correction

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length === 0) {
+const count = parseInt(args[0]);
+if (isNaN(count)) {
     console.log('Missing number of occurrences');
 } else {
-    const count = parseInt(args[0]);
     for (let i = 0; i < count; i++) {
         console.log('C is fun');
     }
